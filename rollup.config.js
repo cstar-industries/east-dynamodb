37fae5ffe287067a883e308bc6296b5dd56be63d
Charles Francoise <charles.francoise@gmail.com>
2021-01-21 14:58:59 +0100

    configure terser plugin

--- rollup.config.js
+++ rollup.config.js
@@ -1,11 +1,12 @@
 import babel from '@rollup/plugin-babel';
+import { terser } from 'rollup-plugin-terser';
 
 export default {
   external: ['path', '@aws-sdk/client-dynamodb', '@aws-sdk/util-dynamodb', /@babel\/runtime/],
   input: 'lib/index.js',
-  plugins: [babel({ babelHelpers: 'runtime' })],
+  plugins: [babel({ babelHelpers: 'runtime' }), terser()],
   output: {
-    file: 'dist/index.cjs',
+    file: 'dist/index.js',
     format: 'cjs',
     exports: 'auto',
     sourcemap: true
