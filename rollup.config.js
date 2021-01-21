fcce2fbfbfe448e6bf6e9b72756c9c7e441d29ae
Charles Francoise <charles.francoise@gmail.com>
2021-01-21 18:21:38 +0100

    copy template file to dist

--- rollup.config.js
+++ rollup.config.js
@@ -1,10 +1,11 @@
 import babel from '@rollup/plugin-babel';
+import copy from 'rollup-plugin-cpy';
 import { terser } from 'rollup-plugin-terser';
 
 export default {
   external: ['path', '@aws-sdk/client-dynamodb', '@aws-sdk/util-dynamodb', /@babel\/runtime/],
   input: 'lib/index.js',
-  plugins: [babel({ babelHelpers: 'runtime' }), terser()],
+  plugins: [babel({ babelHelpers: 'runtime' }), terser(), copy({ files: 'lib/template.js', dest: 'dist' })],
   output: {
     file: 'dist/index.js',
     format: 'cjs',
